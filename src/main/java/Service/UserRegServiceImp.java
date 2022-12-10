cbaf81aaa466b39654aff211c9bd7a4fc0e638ef
Eakteker jahan <eaktekerjahan@gmail.com>
2022-12-11 00:01:47 +0600

    project created with basic functionality2

--- src/main/java/Service/UserRegServiceImp.java
+++ src/main/java/Service/UserRegServiceImp.java
@@ -1,4 +1,41 @@
 package Service;
 
-public class UserRegServiceImp {
+import Registration.Repository;
+import Registration.UserBean;
+
+import java.util.List;
+
+public class UserRegServiceImp implements UserRegService {
+    @Override
+    public  int save(UserBean userBean){
+       Repository repository =  new Repository();
+       boolean existStatement = repository.isUserExitsWithRegistration(userBean.getRegistrationNumber());
+       if (existStatement)
+       {
+           return -1;
+       }
+
+        return repository.saveUserInfo(userBean);
+    }
+    @Override
+    public boolean User_log_in(int registrationNumber ,int password)
+    {
+        Repository repository =  new Repository();
+        return   repository.findUserByCredential(registrationNumber,password);
+
+
+
+    }
+
+    @Override
+    public List<UserBean> getAllUser() {
+        Repository repository =  new Repository();
+         return  repository.getAllUser();
+    }
+
+    @Override
+    public UserBean getUserByRegistration(int RegistrationNumber) {
+        Repository repository =  new Repository();
+        return  repository.findUserByRegistration(RegistrationNumber);
+    }
 }
--- src/main/webapp/RegistrationForm_process.jsp
+++ src/main/webapp/RegistrationForm_process.jsp
@@ -1,4 +1,5 @@
-<%--
+<%@ page import="Service.UserRegService" %>
+<%@ page import="Service.UserRegServiceImp" %><%--
   Created by IntelliJ IDEA.
   User: HP
   Date: 10/12/2022
@@ -6,11 +7,23 @@
   To change this template use File | Settings | File Templates.
 --%>
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<jsp:useBean id="userBean" class="Registration.UserBean"></jsp:useBean>
+<jsp:setProperty property="*" name="userBean"></jsp:setProperty>
 <html>
 <head>
     <title>Title</title>
 </head>
 <body>
+<%
+ UserRegService userRegService = new UserRegServiceImp();
+ int status = userRegService.save(userBean);
+  if(status==1)
+{
+
+
+}
+
+%>
   
   </body>
 </html>
